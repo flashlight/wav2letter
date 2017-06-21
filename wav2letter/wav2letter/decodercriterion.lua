70e65f85422adf643c8ece24217af1c70b4902b5
Ronan Collobert <locronan@fb.com>
2017-06-21 16:56:11 -0700

    decodercriterion: smoother version of the fallback

--- wav2letter/wav2letter/decodercriterion.lua
+++ wav2letter/wav2letter/decodercriterion.lua
@@ -8,11 +8,14 @@ DecoderCriterion.__init = argcheck{
    {name="dopt", type="table"},
    {name="N", type="number"},
    {name="scale", type="function", default=function(input, target) return 1 end},
+   {name="fbw", type="number", default=0},
    call =
-      function(self, decoder, dopt, N, scale)
+      function(self, decoder, dopt, N, scale, fbw)
+         assert(fbw >= 0 and fbw <= 1)
          assert(N == #decoder.letters+1)
          self.decoder = decoder
          self.dopt = dopt
+         self.__fallbackweight = fbw
          local function falscale(input, target)
             self.__scale = scale(input, target)
             return -self.__scale
@@ -24,8 +27,8 @@ DecoderCriterion.__init = argcheck{
          self.fcc = nn.FullConnectCriterionC(N, false, fccscale)
          self.fcc.transitions = self.fal.transitions
          self.transitions = self.fal.transitions
-         self.gtransitions = self.fal.gtransitions
-         self.gradInput = self.fal.gradInput
+         self.gtransitions = torch.Tensor(N, N)
+         self.gradInput = torch.Tensor()
       end
 }
 
@@ -39,30 +42,32 @@ function DecoderCriterion:updateOutput(input, target)
    local faloutput = self.fal:updateOutput(input, target)
    local scale = self.__scale
 
+   local fbw = self.__fallbackweight
+   -- if fbw > 0 then
+   local fccoutput = self.fcc:updateOutput(input)
+   self.output = fbw*(faloutput+fccoutput)
+   -- else
+   --    self.output = 0
+   -- end
+   -- if fbw < 1 then
    -- we need to add the lm score note: substracting lm score from the
    -- decoder is wrong (decoder finds best path with lm score... so fal
    -- might be higher if lm score is removed there)
    local lmoutput = self.dopt.lmweight*self.decoder.lm:estimate(self.decoder.usridx2lmidx(self.__words))
    lmoutput = lmoutput + self.dopt.wordscore*self.__words:size(1)
    lmoutput = lmoutput*(-scale)
-
+   
    -- we do not clone path (fast but ugly)
    local decoutputs
    self.__predictions, self.__lpredictions, decoutputs
       = self.decoder(self.dopt, self.transitions, input)
-
    local decoutput = scale*decoutputs
-
-   self.output = (faloutput + lmoutput) + decoutput
-
-   -- skip if score is negative (could also use the hint...)
-   if self.output <= 0 then
-      self.__fallback = true
-      self.output = faloutput + self.fcc:updateOutput(input)
-   else
-      self.__fallback = false
-   end
-
+   
+   self.output = self.output + (1-fbw)*math.max(0, (faloutput + lmoutput) + decoutput)
+   self.__uselm = (faloutput + lmoutput) + decoutput > 0
+   -- else
+   --    self.__uselm = false
+   -- end
    return self.output
 end
 
@@ -73,40 +78,43 @@ end
 function DecoderCriterion:zeroGradParameters()
    self.fal:zeroGradParameters()
    self.fcc:zeroGradParameters()
+   self.gtransitions:zero()
 end
 
-function DecoderCriterion:updateGradInput(input, target)
-   -- skip if score is negative
-   if self.__fallback then
-      self.fal:updateGradInput(input, target)
-      self.fcc:updateGradInput(input)
-      self.gradInput:add(self.fcc.gradInput)
-      self.gtransitions:add(self.fcc.gtransitions)
-      return self.gradInput
-   end
-
+function DecoderCriterion:updateGradInput(input, target) 
+   local fbw = self.__fallbackweight
+   self.gradInput:resizeAs(input)
    self.fal:updateGradInput(input, target)
+   self.fcc:updateGradInput(input)
+   self.gradInput:copy(self.fal.gradInput)
+   self.gradInput:add(fbw, self.fcc.gradInput)
 
-   local ginput = self.gradInput
-   local scale = self.__scale
-   local target = self.__lpredictions
-   local gtransitions = self.gtransitions
-   local idxm1
-   local N = target:size(1)-2 -- beware of start/end nodes
-   for t=1,N do
-      -- beware of start/end nodes -- beware of 0 indexing
-      local idx = target[t+1]+1
-      ginput[t][idx] = ginput[t][idx] + scale
-      if idxm1 then
-         gtransitions[idx][idxm1] = gtransitions[idx][idxm1] + scale
+   self.gtransitions:add(self.fal.gtransitions)
+   self.gtransitions:add(fbw, self.fcc.gtransitions)
+   
+   if self.__uselm and fbw < 1 then
+      local ginput = self.gradInput
+      local scale = self.__scale*(1-fbw)
+      local target = self.__lpredictions
+      local gtransitions = self.gtransitions
+      local idxm1
+      local N = target:size(1)-2 -- beware of start/end nodes
+      for t=1,N do
+         -- beware of start/end nodes -- beware of 0 indexing
+         local idx = target[t+1]+1
+         ginput[t][idx] = ginput[t][idx] + scale
+         if idxm1 then
+            gtransitions[idx][idxm1] = gtransitions[idx][idxm1] + scale
+         end
+         idxm1 = idx
       end
-      idxm1 = idx
    end
+   
    return self.gradInput
 end
 
 function DecoderCriterion:updateParameters(lr)
-   self.fal:updateParameters(lr)
+   self.transitions:add(-lr, self.gtransitions)
 end
 
 function DecoderCriterion:parameters()

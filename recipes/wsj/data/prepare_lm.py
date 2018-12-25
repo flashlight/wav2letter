a55fea9af3e97164e198624cace5adf9afab73df
Qiantong Xu <qiantong@fb.com>
2018-12-24 17:51:24 -0800

    wsj lm data prepare
    Summary: as title
    
    Reviewed By: vineelpratap
    
    Differential Revision: D13544122
    
    fbshipit-source-id: e0f19659a0c4c88898589bf4fa2ef11c0af231c9

--- recipes/wsj/data/prepare_lm.py
+++ recipes/wsj/data/prepare_lm.py
@@ -15,6 +15,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import argparse
 import gzip
 import os
+import re
+import subprocess
+import sys
 
 import utils
 
@@ -24,6 +27,7 @@ lmdirs = [
     "13_32.1/wsj1/doc/lng_modl/lm_train/np_data/88",
     "13_32.1/wsj1/doc/lng_modl/lm_train/np_data/89",
 ]
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WSJ LM data creation.")
@@ -46,14 +50,52 @@ if __name__ == "__main__":
     okdict = utils.processdict(args.cmudict)
     notword = {}  # not a word warning
 
-    for dir in lmdirs:
-        dir = os.path.join(args.wsj1, dir)
-        for filename in os.listdir(dir):
-            if not filename.endswith(".z"):
-                continue
-            filename = os.path.join(dir, filename)
-            with gzip.open(filename, "r", encoding="utf-8") as f:
-                for line in f:
-                    line = line.lower()
+    # Form dictionary and save LM training data
+    data_file = os.path.join(args.dst, "data.txt")
+    with open(data_file, "w") as training_data:
+        for dir in lmdirs:
+            dir = os.path.join(args.wsj1, dir)
+            for filename in os.listdir(dir):
+                if not filename.endswith(".z"):
+                    continue
 
-# TODO (vineelkpratap): finish LM processing for WSJ
+                # Get text from zip files
+                filename = os.path.join(dir, filename)
+                process = subprocess.Popen(["zcat", filename], stdout=subprocess.PIPE)
+                out, _ = process.communicate()
+                text = out.decode("utf-8")
+                text = re.sub("<s[^>]+>", "<s>", text)
+                text = re.sub("<s>", "{", text)
+                text = re.sub("</s>", "}", text)
+
+                lines = re.finditer(r"\{(.*?)\}", text, re.MULTILINE | re.DOTALL)
+                for line in lines:
+                    line = line.group(1)
+                    line = line.lower().strip()
+                    line = re.sub(" +", " ", line)
+
+                    sentence = []
+                    for raw_word in line.split():
+                        word, spelling = utils.preprocess(raw_word)
+                        if len(word) > 0:
+                            sentence.append(word)
+                            if len(spelling) > 0 and re.match(r"[a-z']+", spelling):
+                                if word not in dict:
+                                    dict[word] = {"cnt": 0, "spelling": spelling}
+                                dict[word]["cnt"] += 1
+                            else:
+                                if word not in ignore:
+                                    ignore[word] = True
+                                    sys.stdout.write(
+                                        "\n$ ignoring word {}".format(spelling)
+                                    )
+                    training_data.write(" ".join(sentence) + "\n")
+
+    # Sort words according to frequency and save lexicon
+    dict = sorted(dict.items(), key=lambda kv: kv[1]["cnt"], reverse=True)
+    dict_file = os.path.join(args.dst, "lexicon.txt")
+    with open(dict_file, "w") as lexicon:
+        for kv in dict:
+            lexicon.write("{w}\t{s}\n".format(w=kv[0], s=" ".join(kv[1]["spelling"])))
+
+    sys.stdout.write("Done !\n")

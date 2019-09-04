flashlight/wav2letter

commit 1545c7ede6d1772280d687fe42f328cee5ceab71

restructure models reproducing and releasing: seq2seq_tds/librispeech recipe
Summary:
Reorganizing data preparation and recipes releasing:

There will be folder **data** to store scripts for each dataset to prepare audio and text data. These data will be generated by these scripts:
- **audio** folder to store flac files
- **text** folder to store transcriptions only data and data for language model
- **lists** to store lists in the format "sample_id file_path file_size target_transcription" (separated by space)

There will be folder **models** to store results for each paper and how to reproduce:
- scripts how to prepare lexicon, tokens, etc.
- config files to train and decode
- info about snapshots

All models WER/LER listed in the tables inside readme of each dataset, also link to the models are provided to reproduce.

Use this strategy to reorganize seq2seq_tds results on libripseech.

Reviewed By: an918tw, vineelpratap

Differential Revision: D16794251

fbshipit-source-id: e6ab3960799a975768879c6a9a4926548ef33fb4

recipes/models/seq2seq_tds/librispeech/prepare.py
@@ -1,0 +1,121 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+----------
+
+Script to prepare recipe to train/eval model on Librispeech in wav2letter++ pipelines
+
+Command : python3 prepare.py --data_dst [...] --model_dst [...]
+
+Replace [...] with appropriate paths
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import argparse
+import os
+from collections import defaultdict
+
+import sentencepiece as spm
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Librispeech Dataset creation.")
+    parser.add_argument(
+        "--data_dst", help="data destination directory", default="./librispeech"
+    )
+    parser.add_argument(
+        "--model_dst",
+        help="model auxilary files destination directory",
+        default="./seq2seq_tds_librispeech",
+    )
+    parser.add_argument(
+        "-p", "--process", help="# of process for Multiprocessing", default=8, type=int
+    )
+
+    args = parser.parse_args()
+    os.system(
+        "python3 {}/../../../data/librispeech/prepare.py --dst {} -p {}".format(
+            os.path.dirname(os.path.abspath(__file__)), args.data_dst, args.process
+        )
+    )
+
+    subpaths = {
+        "train": ["train-clean-100", "train-clean-360", "train-other-500"],
+        "dev": ["dev-clean", "dev-other"],
+        "test": ["test-clean", "test-other"],
+    }
+
+    lists_path = os.path.join(args.data_dst, "lists")
+    am_path = os.path.join(args.model_dst, "am")
+    decoder_path = os.path.join(args.model_dst, "decoder")
+    os.makedirs(am_path, exist_ok=True)
+    os.makedirs(decoder_path, exist_ok=True)
+
+    # Generating am/*
+    num_wordpieces = 10000
+    nbest = 10
+    train_all_text = os.path.join(am_path, "train.txt")
+    prefix = "tokens-train-all-unigram-{}".format(num_wordpieces)
+    prefix = os.path.join(am_path, prefix)
+    vocab_name = prefix + ".vocab"
+    model_name = prefix + ".model"
+
+    # prepare data
+    print("Preparing tokens and lexicon for acoustic model...\n", flush=True)
+    word_dict = defaultdict(set)
+    with open(train_all_text, "w") as ftext:
+        for key, names in subpaths.items():
+            for name in names:
+                with open(os.path.join(lists_path, name + ".lst"), "r") as flist:
+                    for line in flist:
+                        transcription = line.strip().split(" ")[3:]
+                        if key == "train":
+                            ftext.write(" ".join(transcription) + "\n")
+                        word_dict[key].update(transcription)
+    lexicon_words = sorted(word_dict["train"] | word_dict["dev"])
+
+    # train
+    print("Computing word pieces...\n", flush=True)
+    train_cmd = (
+        "--input={input} --model_prefix={prefix} --vocab_size={sz}"
+        " --character_coverage=1.0 --model_type=unigram"
+        " --split_by_unicode_script=false".format(
+            input=train_all_text, prefix=prefix, sz=num_wordpieces
+        )
+    )
+    spm.SentencePieceTrainer.Train(train_cmd)
+
+    # word piece dictionary
+    print("Creating word piece list...\n", flush=True)
+    exclude_list = {"<unk>", "<s>", "</s>"}
+    with open(vocab_name.replace(".vocab", ".tokens"), "w") as fvocab_filt:
+        with open(vocab_name, "r") as fvocab:
+            for line in fvocab:
+                val, _ = line.strip().split("\t", 1)
+                if val not in exclude_list:
+                    fvocab_filt.write(val.replace("\u2581", "_") + "\n")
+
+    # word -> word piece lexicon for loading targets
+    print("Creating word -> word pieces lexicon...\n", flush=True)
+    sp = spm.SentencePieceProcessor()
+    sp.Load(model_name)
+    lexicon_name = "lexicon-train+dev-unigram-{sz}-nbest{n}.lexicon".format(
+        sz=num_wordpieces, n=nbest
+    )
+    with open(os.path.join(am_path, lexicon_name), "w") as f_lexicon:
+        for word in lexicon_words:
+            wps = sp.NBestEncodeAsPieces(word, nbest)
+            for wp in sorted(wps):
+                f_lexicon.write(
+                    word
+                    + "\t"
+                    + " ".join([w.replace("\u2581", "_") for w in wp])
+                    + "\n"
+                )
+
+    print("Done!", flush=True)

recipes/data/librispeech/prepare.py
@@ -67,7 +67,8 @@ if __name__ == "__main__":
         if not os.path.exists(os.path.join(audio_path, "LibriSpeech", pname)):
             print("Downloading and unpacking {}...".format(pname))
             cmd = """wget -c {http}{name}.tar.gz -P {path};
-                     yes n 2>/dev/null | gunzip {path}/{name}.tar.gz; tar -C {path} -xf {path}/{name}.tar"""
+                     yes n 2>/dev/null | gunzip {path}/{name}.tar.gz;
+                     tar -C {path} -xf {path}/{name}.tar"""
             os.system(cmd.format(path=audio_path, http=audio_http, name=pname))
         else:
             log_str = "{} part of data exists, skip its downloading and unpacking"
@@ -75,7 +76,8 @@ if __name__ == "__main__":
     # Downloading text data for language model training
     if not os.path.exists(os.path.join(text_path, "librispeech-lm-norm.txt")):
         print("Downloading and unpacking text data...")
-        cmd = "wget -c {http} -P {path}; yes n 2>/dev/null | gunzip {path}/librispeech-lm-norm.txt.gz"
+        cmd = """wget -c {http} -P {path}; yes n 2>/dev/null |
+                 gunzip {path}/librispeech-lm-norm.txt.gz"""
         os.system(cmd.format(http=text_http, path=text_path))
     else:
         print("Text data exists, skip its downloading." + LOG_STR, flush=True)

recipes/data/wsj/prepare.py
@@ -190,8 +190,8 @@ if __name__ == "__main__":
         data_dst = os.path.join(audio_path, set_name)
         if os.path.exists(data_dst):
             print(
-                "The folder {} exists, existing flac for this folder will be skipped for generation."
-                " Please remove the folder if you want to regenerate the data".format(
+                """The folder {} exists, existing flac for this folder will be skipped for generation.
+                Please remove the folder if you want to regenerate the data""".format(
                     data_dst
                 ),
                 flush=True,
@@ -248,7 +248,8 @@ if __name__ == "__main__":
         "13{}32.1/wsj1/doc/lng_modl/lm_train/np_data/89".format(wsj1_sep),
     ]
     if not os.path.exists(os.path.join(text_path, "cmudict.0.7a")):
-        cmd = "cd {} && wget http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict.0.7a"
+        url = "http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict.0.7a"
+        cmd = "cd {} && wget {}".format(url)
         os.system(cmd.format(text_path))
     else:
         print("CMU dict already exists, skip its downloading", flush=True)
@@ -289,7 +290,8 @@ if __name__ == "__main__":
                     text_data = out.decode("utf-8")
                     text_data = text_data.lower()
                     # split several sentences into sequence (split if word contains
-                    # dot only at the end and this word is absent in the existed words set)
+                    # dot only at the end and this word is absent
+                    # in the existed words set)
                     text_data = " ".join(
                         [
                             word[:-1] + "\n"

recipes/models/conv_glu/librispeech/prepare.py
@@ -19,10 +19,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import argparse
 import os
-import re
 from collections import defaultdict
-
-import numpy
 
 
 if __name__ == "__main__":
@@ -52,8 +49,6 @@ if __name__ == "__main__":
         "dev": ["dev-clean", "dev-other"],
         "test": ["test-clean", "test-other"],
     }
-
-    path_names = numpy.concatenate(list(subpaths.values()))
 
     lists_path = os.path.join(args.data_dst, "lists")
     am_path = os.path.join(args.model_dst, "am")
@@ -89,48 +84,15 @@ if __name__ == "__main__":
             )
 
     # Generating decoder/*
-    lm = "4-gram"
-    assert os.path.isdir(str(args.kenlm)), "kenlm directory not found - '{d}'".format(
-        d=args.kenlm
-    )
-    print("Downloading Librispeech official LM model...\n", flush=True)
-    arpa_file = os.path.join(decoder_path, lm + ".arpa")
-    if not os.path.exists(arpa_file):
-        os.system(
-            "wget -c -O - http://www.openslr.org/resources/11/{lm}.arpa.gz | "
-            "gunzip -c > {fout}".format(lm=lm, fout=arpa_file)
-        )
-    else:
-        print("Arpa file {} exist, skip its downloading.".format(arpa_file))
-    # temporary arpa file in lowercase
-    print("Saving ARPA LM file in binary format ...\n", flush=True)
+    cmd = [
+        "python3 {}/../../scripts/prepare_librispeech_official_lm.py",
+        "--dst {}",
+        "--kenlm {}",
+    ]
     os.system(
-        "cat {arpa} | tr '[:upper:]' '[:lower:]' > {arpa}.tmp".format(arpa=arpa_file)
-    )
-    binary = os.path.join(args.kenlm, "build", "bin", "build_binary")
-    os.system(
-        "{bin} {farpa}.tmp {fbin}".format(
-            bin=binary, farpa=arpa_file, fbin=arpa_file.replace(".arpa", ".bin")
+        " ".join(cmd).format(
+            os.path.dirname(os.path.abspath(__file__)), decoder_path, args.kenlm
         )
     )
-    os.remove(os.path.join(arpa_file + ".tmp"))
-
-    # prepare lexicon word -> tokens spelling
-    # write words to lexicon.txt file
-    lex_file = os.path.join(decoder_path, "lexicon.txt")
-    print("Writing Lexicon file - {}...".format(lex_file))
-    with open(lex_file, "w") as f:
-        # get all the words in the arpa file
-        with open(arpa_file, "r") as arpa:
-            for line in arpa:
-                # verify if the line corresponds to unigram
-                if not re.match(r"[-]*[0-9\.]+\t\S+\t*[-]*[0-9\.]*$", line):
-                    continue
-                word = line.split("\t")[1]
-                word = word.strip().lower()
-                if word == "<unk>" or word == "<s>" or word == "</s>":
-                    continue
-                assert re.match("^[a-z']+$", word), "invalid word - {w}".format(w=word)
-                f.write("{w}\t{s} |\n".format(w=word, s=" ".join(word)))
 
     print("Done!", flush=True)

recipes/models/conv_glu/wsj/prepare.py
@@ -168,7 +168,8 @@ if __name__ == "__main__":
     binary = os.path.join(args.kenlm, "build", "bin", "build_binary")
     lm_file = os.path.join(decoder_path, "lm-4g")
     os.system(
-        "{bin} -T /tmp -S 10G --discount_fallback -o 4 --text {file} > {lm_file}.arpa".format(
+        """{bin} -T /tmp -S 10G --discount_fallback
+        -o 4 --text {file} > {lm_file}.arpa""".format(
             bin=lmplz, lm_file=lm_file, file=train_data
         )
     )

recipes/models/conv_glu/librispeech/decode_clean.cfg
@@ -4,7 +4,7 @@
 --listdata=true
 --lexicon=[MODEL_DST]/decoder/lexicon.txt
 --lm=[MODEL_DST]/decoder/4-gram.bin
---am=[...]/001_model#dev-clean.bin
+--am=[...]/001_model_dev-clean.bin
 --test=[DATA_DST]/lists/test-clean.lst
 --sclite=[...]
 --lmweight=2.5

recipes/models/conv_glu/wsj/decode.cfg
@@ -3,7 +3,7 @@
 --listdata=true
 --lexicon=[MODEL_DST]/decoder/lexicon.txt
 --lm=[MODEL_DST]/decoder/lm-4g.bin
---am=[...]/001_model#nov93dev.bin
+--am=[...]/001_model_nov93dev.bin
 --test=[DATA_DST]/lists/nov92.lst
 --sclite=[...]
 --lmweight=5.5

recipes/models/conv_glu/wsj/train.cfg
@@ -6,7 +6,7 @@
 --archdir=[...]
 --listdata=true
 --train=[DATA_DST]/lists/si284.lst
---valid=[DATA_DST]/lists/nov93dev.lst
+--valid=nov93dev:[DATA_DST]/lists/nov93dev.lst
 --lexicon=[MODEL_DST]/am/lexicon_si284+nov93dev.txt
 --arch=network.arch
 --tokens=tokens.txt

commit cfbd32cad980af9a90bb7494da08a79588b39a9b

fix original ordering of spellings for the same word
Summary:
Thanks an918tw for pointing this bug introduced in 1545c7ede6d1772280d687fe42f328cee5ceab71.

Preserve ordering of spellings in lexicon (as in the original paper).
- During training we sample spelling taking mostly the first segmentaiton.
- This affects for the total length of the sequence

Reviewed By: an918tw

Differential Revision: D17188028

fbshipit-source-id: 49f6a927ef85ca93338895034da9f076fe298d7e

recipes/models/seq2seq_tds/librispeech/prepare.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     with open(os.path.join(am_path, lexicon_name), "w") as f_lexicon:
         for word in lexicon_words:
             wps = sp.NBestEncodeAsPieces(word, nbest)
-            for wp in sorted(wps):
+            for wp in wps: # the order matters for our training 
                 f_lexicon.write(
                     word
                     + "\t"
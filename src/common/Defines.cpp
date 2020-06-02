61ad71d504292263548b5a8f2f911e689d1922ed
Tatiana Likhomanenko <antares@fb.com>
2020-06-02 12:48:44 -0700

    set default warmup=0 to have reproducible config for non-transformer models
    Summary: Set default warmup=0 for reproducibility of old configs
    
    Reviewed By: vineelpratap
    
    Differential Revision: D21835421
    
    fbshipit-source-id: 8033fae650e3100e2ee0181fa937476d2852ff6e

--- src/common/Defines.cpp
+++ src/common/Defines.cpp
@@ -81,7 +81,7 @@ DEFINE_double(lr, 1.0, "learning rate");
 DEFINE_double(momentum, 0.0, "momentum factor");
 DEFINE_double(weightdecay, 0.0, "weight decay (L2 penalty)");
 DEFINE_double(lrcrit, 0, "criterion learning rate");
-DEFINE_int64(warmup, 8000, "the LR warmup parameter, in updates");
+DEFINE_int64(warmup, 1, "the LR warmup parameter, in updates");
 DEFINE_int64(
     saug_start_update,
     -1,

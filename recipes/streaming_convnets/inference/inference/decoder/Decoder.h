da389ca10383c270c638b2084e46925005489fc9
Jacob Kahn <jacobkahn@fb.com>
2021-08-05 16:43:01 -0700

    Switch license to MIT (#982)
    Summary:
    Pull Request resolved: https://github.com/flashlight/wav2letter/pull/982
    
    Swap licensing to use MIT rather than BSD.
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D30136481
    
    fbshipit-source-id: f530f512c70b9232160823a3e10e2d376047ad43

--- recipes/streaming_convnets/inference/inference/decoder/Decoder.h
+++ recipes/streaming_convnets/inference/inference/decoder/Decoder.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018-present, Facebook, Inc.
  *
- * This source code is licensed under the BSD-style license found in the
+ * This source code is licensed under the MIT-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */

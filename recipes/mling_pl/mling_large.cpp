004e3cc473dc77cff911b78193d4902d8068f06a
loren.lugosch <loren.lugosch@gmail.com>
2022-03-01 16:02:24 -0800

    adding colab and small fixes for multilingual model recipe (#1003)
    Summary:
    **IMPORTANT: Please do not create a Pull Request without creating an issue first.** Changes *must* be discussed.
    
    **Original Issue**: n/a
    
    *Note:* You can add `closes #[issue number]` to automatically close the issue that this PR resolves when it is merged.
    
    adding colab and small fixes for multilingual model recipe
    
    ### Test Plan (required)
    n/a
    
    Pull Request resolved: https://github.com/flashlight/wav2letter/pull/1003
    
    Differential Revision: D34379954
    
    Pulled By: vineelpratap
    
    fbshipit-source-id: e032cad89808ee19467fcff112b3fda601894d15

--- recipes/mling_pl/mling_large.cpp
+++ recipes/mling_pl/mling_large.cpp
@@ -59,63 +59,17 @@ class myModel : public fl::Container {
       const std::vector<fl::Variable>& input) override {
     auto out = input[0];
     auto xSizes = input[1].array();
-    float reshaping_factor = 1;
-    if (input.size() > 2) {
-      reshaping_factor = af::sum<float>(input[2].array());
-    }
     float dp = -1;
-    if (input.size() > 3) {
-      dp = af::sum<float>(input[3].array());
+    if (input.size() > 2) {
+      dp = af::sum<float>(input[2].array());
     }
     // expected input dims T x C x 1 x B
     out = convFrontend_->forward(out);
-    ///////// reshape ////////
-    int time_dim = 1, feat_dim = 0, other_dim = 3, batch_dim = 2;
-    int old_B = out.dims(batch_dim);
-    int old_T = out.dims(time_dim);
-    int new_B = old_B;
-    int new_T = old_T;
-    int T_padded = old_T;
-    if (reshaping_factor != 1) {
-      new_T = ceil(reshaping_factor * old_T);
-      new_T += old_B -
-          (new_T % old_B); // add this chunk so that new_T is divisible by old_B
-      new_B = ceil((float)(old_B * old_T) / (float)new_T);
-      T_padded = (new_B * new_T) / old_B;
-      std::vector<std::pair<int, int>> pad_amount;
-      pad_amount.push_back(std::make_pair(0, 0));
-      pad_amount.push_back(std::make_pair(0, T_padded - old_T));
-      pad_amount.push_back(std::make_pair(0, 0));
-      pad_amount.push_back(std::make_pair(0, 0));
-      out = fl::padding(out, pad_amount, 0.0);
-      out = fl::reorder(out, time_dim, batch_dim, feat_dim, other_dim);
-      time_dim = 0, feat_dim = 2, other_dim = 3, batch_dim = 1;
-      auto new_out_dims = out.dims();
-      new_out_dims[time_dim] = new_T;
-      new_out_dims[batch_dim] = new_B;
-      out = fl::moddims(out, new_out_dims);
-      out = fl::reorder(out, feat_dim, time_dim, batch_dim, other_dim);
-      //   std::cout << "(reshaping)\n";
-    } else {
-      //   std::cout << "(not reshaping)\n";
-    }
-    // std::cout << "old_B: " << old_B << "\n";
-    // std::cout << "old_T: " << old_T << "\n";
-    // std::cout << "new_B: " << new_B << "\n";
-    // std::cout << "new_T: " << new_T << "\n";
-    // std::cout << "T_padded: " << T_padded << "\n";
-    if (T_padded * old_B != new_T * new_B) {
-      std::cout << "error, T_padded * old_B != new_T * new_B\n";
-      exit(0);
-    }
-    //////////////////////////
-    af::array inputNotPaddedSize(1, old_B, 1, 1);
-    for (int bIdx = 0; bIdx < old_B; bIdx++) {
-      inputNotPaddedSize(0, bIdx, 0, 0) = old_T;
-    } // TODO: use actual xSizes here
-    auto padMask = af::iota(af::dim4(T_padded, 1), af::dim4(1, old_B)) <
-        af::tile(inputNotPaddedSize, T_padded, 1);
-    padMask = af::moddims(padMask, af::dim4(new_T, new_B, 1, 1));
+    int T = out.dims(0), B = out.dims(3);
+    auto inputMaxSize = af::tile(af::max(xSizes), 1, B);
+    af::array inputNotPaddedSize = af::ceil(xSizes * T / inputMaxSize);
+    auto padMask = af::iota(af::dim4(T, 1), af::dim4(1, B)) <
+        af::tile(inputNotPaddedSize, T, 1);
     for (int trIdx = 0; trIdx < transformers_.size(); trIdx++) {
       // NOTE: not required for inference
       //   if (dp >= 0) {
@@ -124,19 +78,7 @@ class myModel : public fl::Container {
       //   }
       out = transformers_[trIdx]->forward({out, fl::noGrad(padMask)}).front();
     }
-    ///////// reshape ////////
-    if (reshaping_factor != 1) {
-      time_dim = 1, feat_dim = 0, other_dim = 3, batch_dim = 2;
-      out = fl::reorder(out, time_dim, batch_dim, feat_dim, other_dim);
-      time_dim = 0, feat_dim = 2, other_dim = 3, batch_dim = 1;
-      auto new_tr_out_dims = out.dims();
-      new_tr_out_dims[time_dim] = T_padded;
-      new_tr_out_dims[batch_dim] = old_B;
-      out = fl::moddims(out, new_tr_out_dims);
-      out = fl::reorder(out, feat_dim, time_dim, batch_dim, other_dim);
-      out = out(af::span, af::seq(old_T), af::span, af::span);
-    }
-    //////////////////////////
+
     auto ctc_head_out = linear_->forward(out);
     auto LID_head_out = LID_head_->forward(out);
     LID_head_out = fl::mean(LID_head_out.as(f32), std::vector<int>{1}).as(f32);

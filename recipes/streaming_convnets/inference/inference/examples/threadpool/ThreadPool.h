2045b14ce2a2e5ef570fa96bdde44046532af1e2
Richard Barnes <rbarnes@meta.com>
2024-05-05 22:01:14 -0700

    Change `result_of` to `invoke_result` in deeplearning/projects/wav2letter/recipes/streaming_convnets/inference/inference/examples/threadpool/ThreadPool.h
    Summary: C++20 has [eliminated](https://en.cppreference.com/w/cpp/types/result_of) `result_of` in favour of `invoke_result`. It's mysterious that this code even still works, but, nevertheless, I'm fixing it.
    
    Differential Revision: D56987415
    
    fbshipit-source-id: 47934eb72362585eb7c65d55b2eafb01e6cf139f

--- recipes/streaming_convnets/inference/inference/examples/threadpool/ThreadPool.h
+++ recipes/streaming_convnets/inference/inference/examples/threadpool/ThreadPool.h
@@ -58,7 +58,7 @@ class ThreadPool {
    */
   template <class F, class... Args>
   auto enqueue(F&& f, Args&&... args)
-      -> std::future<typename std::result_of<F(Args...)>::type>;
+      -> std::future<typename std::invoke_result<F, Args...>::type>;
   ///  destructor joins all threads.
   ~ThreadPool();
 
@@ -103,8 +103,8 @@ inline ThreadPool::ThreadPool(
 
 template <class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args)
-    -> std::future<typename std::result_of<F(Args...)>::type> {
-  using return_type = typename std::result_of<F(Args...)>::type;
+    -> std::future<typename std::invoke_result<F, Args...>::type> {
+  using return_type = typename std::invoke_result<F, Args...>::type;
 
   auto task = std::make_shared<std::packaged_task<return_type()>>(
       std::bind(std::forward<F>(f), std::forward<Args>(args)...));
